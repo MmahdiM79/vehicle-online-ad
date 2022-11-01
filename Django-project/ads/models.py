75c882dba26cfe31765ebe8590ee3ae940855af0
Mohammad Mahdi Malmasi <m.mahdi.m79@gmail.com>
2022-11-01 09:23:56 +0330

    VehicleAD model added to ads app

--- Django-project/ads/models.py
+++ Django-project/ads/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class VehicleAD(models.Model):
+    class StateAD(models.TextChoices):
+        ACCEPTED = 'accepted'
+        REVIEW = 'review'
+        REJECTED = 'rejected'
+
+    description = models.CharField(max_length=4096, default='')
+    state = models.CharField(
+        max_length=10,
+        choices=StateAD.choices,
+        default=StateAD.REVIEW,
+        null=False
+    )
+    image = models.CharField(max_length=1024, null=False, blank=False)
+    email = models.CharField(max_length=2048, null=False, blank=False)
+
+    def __str__(self):
+        return self.title

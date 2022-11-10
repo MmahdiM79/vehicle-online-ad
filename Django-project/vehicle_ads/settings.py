7b4ec7f1e9064fa566f568fbe4a88dadc7500ba3
Mohammad Mahdi Malmasi <m.mahdi.m79@gmail.com>
2022-11-10 19:43:35 +0330

    valid catogories added

--- Django-project/vehicle_ads/settings.py
+++ Django-project/vehicle_ads/settings.py
@@ -135,6 +135,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ''' additional settings '''
 
+VALID_CATEGORIES = [
+    'car',
+    'vehicle',
+    'automobile',
+    'motor vehicle',
+    'motorcar',
+    'auto',
+    'automotive vehicle',
+    'machine',
+    'bike',
+    'motorcycle',
+    'motorbike',
+    'bicycle',
+    'moped',
+]
+
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
